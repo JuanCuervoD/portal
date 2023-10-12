b5ff92328176d136bc254a64e32c3210a3a4d8b5
Juan Pablo Cuervo D <67351670+JuanCuervoD@users.noreply.github.com>
2023-10-12 04:29:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Inicializa Swiper
+const swiper = new Swiper('.swiper-container', {
+    loop: true, // Para que el carrusel sea infinito
+    autoplay: {
+        delay: 10000, // Cambia la imagen cada 5 segundos
+    }
+});
+
+
